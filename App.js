42450f74de502708ebeaa577e05455ed4b79a04f
dschwark1111 <135233905+dschwark1111@users.noreply.github.com>
2023-07-31 21:00:48 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Catalog from './pages/catalog';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Catalog/>
+      <Footer/>
+
+    </div>
+    
+    
+  );
+}
+
+export default App;
